803d6c325af9554bae27af9e4652c4873b13254c
wshuts <shutswilliam@gmail.com>
2022-02-19 01:08:59 -0600

    drop_last failing test

--- its-your-turn-125.js
+++ its-your-turn-125.js
@@ -1,4 +1,8 @@
-pop_example();
+const functions = {
+    pop_example: pop_example,
+    last_element: last_element,
+    drop_last: drop_last
+};
 
 function pop_example(){
     var a = [1, 2, 3, 4];
@@ -11,4 +15,10 @@ function last_element(array){
     return array[array.length - 1];
 }
 
-module.exports = last_element;
+function drop_last(array){
+    var array_copy = array.slice();
+    array_copy.pop();
+    return array_copy;
+}
+
+module.exports = functions;
--- its-your-turn-125.test.js
+++ its-your-turn-125.test.js
@@ -1,8 +1,12 @@
-const last_element = require('./its-your-turn-125');
+const functions = require('./its-your-turn-125');
 
 test('reads the last element of an array', () => {
-    expect(last_element([1, 2, 3, 4])).toBe(4);
-    expect(last_element([1, 2, 3])).toBe(3);
-    expect(last_element([1])).toBe(1);
-    expect(last_element([])).toBe(undefined);
+    expect(functions.last_element([1, 2, 3, 4])).toBe(4);
+    expect(functions.last_element([1, 2, 3])).toBe(3);
+    expect(functions.last_element([1])).toBe(1);
+    expect(functions.last_element([])).toBe(undefined);
+});
+
+test('drops the last element of an array', () => {
+    expect(functions.drop_last([1, 2, 3, 4])).toBe([1, 2, 3, 4]);
 });
