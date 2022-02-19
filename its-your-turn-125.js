cf951753507559ef74f11468cb5f002a797756d9
wshuts <shutswilliam@gmail.com>
2022-02-18 23:58:21 -0600

    begin last_element

---
+++ its-your-turn-125.js
@@ -0,0 +1,12 @@
+pop_example();
+
+function pop_example(){
+    var a = [1, 2, 3, 4];
+    var b = a.pop();
+    console.log(b); // prints 4
+    console.log(a); // prints [1, 2, 3]
+}
+
+function last_element(array){
+
+}
