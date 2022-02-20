321ee90b4118d6a51e634934e21eade83d68a089
wshuts <shutswilliam@gmail.com>
2022-02-20 12:32:55 -0600

    objectSet passing test

--- its-your-turn-136.js
+++ its-your-turn-136.js
@@ -2,8 +2,10 @@ const functions = {
     objectSet: objectSet
 };
 
-function objectSet(){
-
+function objectSet(object, key, value) {
+    var object_copy = Object.assign({}, object);
+    object_copy[key] = value;
+    return object_copy;
 }
 
 module.exports = functions;
