2987b5106f9717f87f9346b5ae0d6fc50222f0c2
play.js <git@gituser.com>
2022-03-05 15:01:37 -0600

    passing tests: push, drop_last, drop_first callbacks

--- its-your-turn-273.js
+++ its-your-turn-273.js
@@ -1,18 +1,38 @@
 const functions = {
-  arraySet: arraySet
+  arraySet: arraySet,
+  push: push,
+  drop_last: drop_last,
+  drop_first: drop_first
 };
-
-function withArrayCopy(array, modifier) {
-  copy = array.slice();
-  modifier(copy);
-  return copy;
-}
 
 function arraySet(array, index, value) {
   return withArrayCopy(array, function(copy) {
     copy[index] = value;
   })
 }
- 
+
+function push(array, element) {
+  return withArrayCopy(array, function(copy) {
+    copy.push(element);
+  })
+}
+
+function drop_last(array) {
+  return withArrayCopy(array, function(copy) {
+    copy.pop();
+  })
+}
+
+function drop_first(array) {
+  return withArrayCopy(array, function(copy) {
+    copy.shift();
+  })
+}
+
+function withArrayCopy(array, modifier) {
+  copy = array.slice();
+  modifier(copy);
+  return copy;
+}
 
 module.exports = functions;
--- its-your-turn-273.test.js
+++ its-your-turn-273.test.js
@@ -2,17 +2,36 @@ const functions = require('./its-your-turn-273');
 
 var array1 = [0, 1, 2];
 var array2 = [0, 1, 9];
-
-test('arraySet as callback', () => {
-    expect(functions.arraySet(array1, 2, 9)).toStrictEqual(array2);
-});
+var array3 = [0, 1, 2, 99];
+var array4 = [0, 1];
+var array5 = [1, 2];
 
 var sequence = function(array1) {
   functions.arraySet(array1, 2, 9);
   return array1;
 };
 
+test('arraySet as callback', () => {
+    expect(functions.arraySet(array1, 2, 9))
+    .toStrictEqual(array2);
+});
+
 test('array1 is unchanged', () => {
     expect(sequence(array1))
     .toStrictEqual(array1);
+});
+
+test('push callback', () => {
+    expect(functions.push(array1, 99))
+    .toStrictEqual(array3);
+});
+
+test('drop_last callback', () => {
+    expect(functions.drop_last(array1))
+    .toStrictEqual(array4);
+});
+
+test('drop_first callback', () => {
+    expect(functions.drop_first(array1))
+    .toStrictEqual(array5);
 });
