e5ab3c178e19f9f1dbbe2dad904e867f02b1bd2b
play.js <git@gituser.com>
2022-03-09 14:19:52 -0600

    passing tests: throwsError, numOfArgs

--- its-your-turn-285.test.js
+++ its-your-turn-285.test.js
@@ -1,8 +1,33 @@
 const functions = require('./its-your-turn-285');
 
-var procedureWithErrorMock = jest.fn(functions.procedureWithError);
+var throwsErrorTestSequence = () => {
+  
+  var procedureWithErrorMock = jest.fn(functions.procedureWithError);
+  
+  return procedureWithErrorMock(0, 2, 3);
+  
+};
 
 test('throws error', () => {
-  expect(() => procedureWithErrorMock(0, 2, 3))
+  expect(throwsErrorTestSequence)
   .toThrow('Error!');
+});
+
+var numOfArgsTestSequence = () => {
+  
+  var procedureWithErrorMock = jest.fn(functions.procedureWithError);
+  
+  procedureWithErrorMock(1, 2, 3);
+  
+  var mockState = procedureWithErrorMock.mock;
+  var mockCalls = mockState.calls;
+  var firstMockCall = mockCalls[0];
+  var numOfArgsForFirstMockCall = firstMockCall.length;
+  return numOfArgsForFirstMockCall;
+  
+  };
+
+test('number of args > 2', () => {
+  expect(numOfArgsTestSequence())
+  .toBeGreaterThan(2);
 });
