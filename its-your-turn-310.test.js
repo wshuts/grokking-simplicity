05fe75189fd93d36f906672734c37f483a95a01e
wshuts <shutswilliam@gmail.com>
2022-03-17 18:51:47 -0500

    passing tests: min, max for array of numbers

--- its-your-turn-310.test.js
+++ its-your-turn-310.test.js
@@ -9,3 +9,13 @@ test('return Number.MIN_VALUE if array is empty', () => {
   expect(functions.max([]))
   .toStrictEqual(Number.MIN_VALUE);
 });
+
+test('return min for array of numbers', () => {
+    expect(functions.min([9, 2, 5, 6, -1, 8, 3]))
+    .toStrictEqual(-1);
+  });
+  
+  test('return max for array of numbers', () => {
+    expect(functions.max([0, 2, 5, 6, -1, 88, 3]))
+    .toStrictEqual(88);
+  });
