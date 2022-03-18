806a08c9b6020782fce9bb6f6a4f96810eaba99a
wshuts <shutswilliam@gmail.com>
2022-03-17 21:37:38 -0500

    arrayLibrary: "corner" cases

---
+++ its-your-turn-312.test.js
@@ -0,0 +1,33 @@
+const arrayLibrary = require('./arrayLibrary');
+
+test('map given empty array', () => {
+    expect(arrayLibrary.map([], x => x * 2))
+    .toStrictEqual([]);
+});
+
+test('filter given empty array', () => {
+    expect(arrayLibrary.filter([], x => x >= 0))
+    .toStrictEqual([]);
+});
+
+test('reduce given empty array', () => {
+    expect(arrayLibrary.reduce([], 0, (acc, x) => acc + x))
+    .toStrictEqual(0);
+});
+
+const numbers = [1, 2, 3, 4, 5];
+
+test('map given x => x', () => {
+    expect(arrayLibrary.map(numbers, x => x))
+    .toStrictEqual(numbers);
+});
+
+test('filter given true', () => {
+    expect(arrayLibrary.filter(numbers, _x => true))
+    .toStrictEqual(numbers);
+});
+
+test('filter given true', () => {
+    expect(arrayLibrary.filter(numbers, _x => false))
+    .toStrictEqual([]);
+});
