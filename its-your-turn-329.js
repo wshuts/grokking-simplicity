bc288715d0f800733f381d89104171f674e4575a
wshuts <shutswilliam@gmail.com>
2022-03-28 23:01:46 -0500

    passing test: averagePurchaseTotals

--- its-your-turn-329.js
+++ its-your-turn-329.js
@@ -1,12 +1,26 @@
 const arrayLibrary = require('./arrayLibrary');
 
 const functions = {
-    average: average
+    average: average,
+    averagePurchaseTotals: averagePurchaseTotals
 }
 
 function average(numbers) {
     var sum = arrayLibrary.reduce(numbers, 0, (sum, number) => sum + number);
     return sum / numbers.length;
+}
+
+function averagePurchaseTotals(customers) {
+    var purchases = arrayLibrary.map(customers, getPurchases);
+    return arrayLibrary.map(purchases, averageTotal);
+}
+
+function getPurchases(customer) {
+    return customer.purchases;
+}
+
+function averageTotal(purchases) {
+    return arrayLibrary.reduce(purchases, 0, (sum, purchase) => sum + purchase.total) / purchases.length;
 }
 
 module.exports = functions;
--- its-your-turn-329.test.js
+++ its-your-turn-329.test.js
@@ -8,5 +8,23 @@ test('Compute an average', () => {
     .toStrictEqual(average);
 });
 
+const purchase01 = {total: 10};
+const purchase02 = {total: 20};
+const purchase03 = {total: 30};
+const purchase04 = {total: 110};
+
+const customer01 = {purchases: [purchase01, purchase02, purchase03]};
+const customer02 = {purchases: [purchase04]};
+const customer03 = {purchases: [purchase01, purchase04]};
+
+const customers = [customer01, customer02, customer03];
+
+const averagePurchaseTotals = [20, 110, 60];
+
+test('Compute the average purchase total for each customer', () => {
+    expect(functions.averagePurchaseTotals(customers))
+    .toStrictEqual(averagePurchaseTotals);
+});
+
 
 
