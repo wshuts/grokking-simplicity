2157b99b6c2d6f2e5717b0b6aa1f8db3527ad4da
wshuts <shutswilliam@gmail.com>
2022-04-02 15:14:00 -0500

    progress: 3 untested helper functions

--- its-your-turn-348.js
+++ its-your-turn-348.js
@@ -8,4 +8,16 @@ function buildRoster(evaluations) {
     return arrayLibrary.reduce(evaluations, [], (roster, evaluation) => []);
 }
 
+function filterByPosition(evaluations, position) {
+    return arrayLibrary.filter(evaluations, evaluation => evaluation.position === position);
+}
+
+function getBest(evaluations) {
+    return arrayLibrary.reduce(evaluations, {name: 'open', position: 'n/a', score: 0}, compareByScore);
+}
+
+function compareByScore(bestEvaluation, evaluation) {
+    return evaluation.score > bestEvaluation.score ? evaluation : bestEvaluation;
+}
+
 module.exports = functions;
