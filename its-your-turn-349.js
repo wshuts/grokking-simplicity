259985e3816203e6312de89220ddadb6b6a0d950
wshuts <shutswilliam@gmail.com>
2022-04-05 21:51:15 -0500

    passing test: buildRecommendations using map

--- its-your-turn-349.js
+++ its-your-turn-349.js
@@ -6,13 +6,33 @@ const functions = {
 }
 
 function buildRecommendations(employeeNames) {
-    var recommendations = [];
-    return recommendations;
+    return arrayLibrary.map(employeeNames, employeeName => {
+        return {
+            name: employeeName,
+            position: recommendPosition(employeeName)
+        };
+    });
 }
 
+const recommendations = [
+    {name: 'Drusilla', position: 'third base'},
+    {name: 'Micah', position: 'center field'},
+    {name: 'Charlie', position: 'pitcher'},
+    {name: 'Marco', position: 'first base'},
+    {name: 'Paula', position: 'short stop'},
+    {name: 'Raoul', position: 'pitcher'},
+    {name: 'Luigi', position: 'first base'},
+    {name: 'Daniela', position: 'right field'},
+    {name: 'Rupert', position: 'second base'},
+    {name: 'Monica', position: 'left field'},
+    {name: 'Bernie', position: 'catcher'},
+    {name: 'Quinn', position: 'short stop'}
+];
+
 function recommendPosition(employeeName) {
-    var recommendation = '';
-    return recommendation;
+    var recommendation = arrayLibrary.filter(recommendations,
+        recommendation => recommendation.name === employeeName);
+    return recommendation[0].position;
 }
 
 module.exports = functions;
