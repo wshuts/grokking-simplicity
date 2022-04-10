51729593bc0e4fb58128c25efb4c0f3098a51cd2
wshuts <shutswilliam@gmail.com>
2022-04-10 01:27:29 -0500

    passing tests: more uses for update

--- its-your-turn-364.js
+++ its-your-turn-364.js
@@ -3,11 +3,14 @@ const objectLibrary = require('./objectLibrary');
 
 
 const functions = {
-    modifyEmailToLowercase: modifyEmailToLowercase,
-    tenXQuantity: tenXQuantity
+    emailToLowercase: emailToLowercase,
+    tenXQuantity: tenXQuantity,
+    incrementScore: incrementScore,
+    decrementLogins: decrementLogins,
+    firstNameToUppercase: firstNameToUppercase
 }
 
-function modifyEmailToLowercase(user) {
+function emailToLowercase(user) {
     return update(user, 'email', lowercase);
 }
 
@@ -21,6 +24,30 @@ function tenXQuantity(item) {
 
 function tenX(quantity) {
     return quantity * 10;
+}
+
+function incrementScore(user) {
+    return update(user, 'score', increment);
+}
+
+function increment(score) {
+    return score + 1;
+}
+
+function decrementLogins(user) {
+    return update(user, 'logins', decrement);
+}
+
+function decrement(logins) {
+    return logins - 1;
+}
+
+function firstNameToUppercase(user) {
+    return update(user, 'firstName', uppercase);
+}
+
+function uppercase(firstName) {
+    return lodash.toUpper(firstName);
 }
 
 function update(object, key, modifier) {
--- its-your-turn-364.test.js
+++ its-your-turn-364.test.js
@@ -1,20 +1,20 @@
 const functions = require('./its-your-turn-364.js');
 
-const userWithUppercaseEmail = {
+const userWithUpperCaseEmail = {
     firstName: 'Joe',
     lastName: 'Nash',
     email: 'JOE@EXAMPLE.COM'
 };
 
-const userWithLowercaseEmail = {
+const userWithLowerCaseEmail = {
     firstName: 'Joe',
     lastName: 'Nash',
     email: 'joe@example.com'
 };
 
 test("user email to lowercase", () => {
-    expect(functions.modifyEmailToLowercase(userWithUppercaseEmail))
-    .toStrictEqual(userWithLowercaseEmail);
+    expect(functions.emailToLowercase(userWithUpperCaseEmail))
+    .toStrictEqual(userWithLowerCaseEmail);
 });
 
 const item = {
@@ -32,4 +32,51 @@ const itemWithtenXQuantity = {
 test("item quantity to 10x", () => {
     expect(functions.tenXQuantity(item))
     .toStrictEqual(itemWithtenXQuantity);
+});
+
+const user = {
+    firstName: 'Cindy',
+    lastName: 'Sullivan',
+    email: 'cindy@randomemail.com',
+    score: 15,
+    logins: 3
+};
+
+const userWithUpdatedScore = {
+    firstName: 'Cindy',
+    lastName: 'Sullivan',
+    email: 'cindy@randomemail.com',
+    score: 16,
+    logins: 3
+};
+
+test("user score incremented", () => {
+    expect(functions.incrementScore(user))
+    .toStrictEqual(userWithUpdatedScore);
+});
+
+const userWithUpdatedLogins = {
+    firstName: 'Cindy',
+    lastName: 'Sullivan',
+    email: 'cindy@randomemail.com',
+    score: 15,
+    logins: 2
+};
+
+test("user logins decremented", () => {
+    expect(functions.decrementLogins(user))
+    .toStrictEqual(userWithUpdatedLogins);
+});
+
+const userWithUpdatedFirstName = {
+    firstName: 'CINDY',
+    lastName: 'Sullivan',
+    email: 'cindy@randomemail.com',
+    score: 15,
+    logins: 3
+};
+
+test("user firstName to upper case", () => {
+    expect(functions.firstNameToUppercase(user))
+    .toStrictEqual(userWithUpdatedFirstName);
 });
